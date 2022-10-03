5aeed77b65e84e9237f5bef383f4c84706a7c5a1
chenle2002 <chenle2020@mail.ynu.edu.cn>
2022-10-03 14:47:07 +0800

    项目初始结构

---
+++ gulimall-member/src/test/java/com/chenle/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenle.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
